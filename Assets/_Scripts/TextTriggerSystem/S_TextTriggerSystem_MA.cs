26bab47d33702e570b1d07a12c4f427bc91e51bc
ingermarie04 <ingermarie04@gmail.com>
2024-11-21 13:37:48 +0100

    Fonts and fonts and useful stuff too like one singular image

--- Assets/_Scripts/TextTriggerSystem/S_TextTriggerSystem_MA.cs
+++ Assets/_Scripts/TextTriggerSystem/S_TextTriggerSystem_MA.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class S_TextTriggerSystem_MA : MonoBehaviour
 {
     [SerializeField] List<string> dialogueText = new List<string>();
-    int textsInList;
+    int displayNumber;
 
-    [SerializeField] TMP_Text textBox;
-    
+    TMP_Text textBox;
+    bool textCanProgress;
+    bool hasHappened;
+
+    void Start()
+    {
+        textBox = FindFirstObjectByType<TMP_Text>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        textCanProgress = true;
+        
+        if(!hasHappened)
+        {
+            textBox.gameObject.SetActive(true);
+            DisplayText();
+            hasHappened = true;
+        }
+    }
 
     private void OnAttack()
     {
-        textBox.text = dialogueText[textsInList];
+        DisplayText();
+    }
 
-        if(textsInList >= dialogueText.Count)
+    void DisplayText()
+    {
+        if (textCanProgress && dialogueText.Count > displayNumber)
         {
+            textBox.text = dialogueText[displayNumber];
+
+            displayNumber++;
+        }
+        else if (textCanProgress && dialogueText.Count >= displayNumber)
+        {
+            textBox.gameObject.SetActive(false);
             Destroy(gameObject);
         }
-        textsInList++;
     }
 }
