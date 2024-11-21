04c7b70ae95737080382ec74c5f0dfa24e5242c6
masterITS <ingesela@gmail.com>
2024-11-21 11:55:01 +0100

    New player movement and added rotation

--- Assets/_Scripts/Player/S_PlayerMovement_IS.cs
+++ Assets/_Scripts/Player/S_PlayerMovement_IS.cs
@@ -3,14 +3,15 @@ using UnityEngine.InputSystem;
 
 public class S_PlayerMovement_IS : MonoBehaviour
 {
-    [SerializeField] private float speed = 6.0f;
-    [SerializeField] private float jumpForce = 8.0f;
-    [SerializeField] private float gravity = 20.0f;
-    [SerializeField] private float rotationSpeed = 1.0f;
+    [SerializeField] private float speed = 6f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float gravity = 20f;
     
     private float _movementX;
     private float _movementY;
     private float _ySpeed;
+
+    private Vector2 _rotationVector;
     
     private Vector3 _moveDirection = Vector3.zero;
     private CharacterController _controller;
@@ -25,6 +26,9 @@ public class S_PlayerMovement_IS : MonoBehaviour
         Vector2 input = inputValue.Get<Vector2>();
         _movementX = input.x;
         _movementY = input.y;
+
+        if (_movementX == 0 && _movementY == 0) return;
+        transform.rotation = Quaternion.Euler(0, GetRotation(), 0);
     }
     
     private void OnJump()
@@ -42,7 +46,6 @@ public class S_PlayerMovement_IS : MonoBehaviour
             _ySpeed -= gravity * Time.deltaTime;
         }
         _moveDirection = new Vector3(_movementX, _ySpeed, _movementY);
-        _moveDirection = transform.TransformDirection(_moveDirection);
         _moveDirection *= speed;
         _controller.Move(_moveDirection * Time.deltaTime);
         
@@ -50,5 +53,17 @@ public class S_PlayerMovement_IS : MonoBehaviour
         {
             _ySpeed = -1f;
         }
+    }
+
+    private int GetRotation()
+    {
+        _rotationVector.x = _movementX;
+        _rotationVector.y = _movementY;
+        _rotationVector.Normalize();
+
+        if (_rotationVector == Vector2.zero) return 0;
+        float angle = Mathf.Atan2(_rotationVector.x, _rotationVector.y) * Mathf.Rad2Deg;
+        return (int) angle;
+
     }
 }
