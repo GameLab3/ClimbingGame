5a56d96a21313c0f11d8a63772703af3f1fa91fa
masterITS <ingesela@gmail.com>
2024-11-27 15:19:02 +0100

    Update S_CheckPoint_IS.cs

--- Assets/_Scripts/Player/Respawing/S_CheckPoint_IS.cs
+++ Assets/_Scripts/Player/Respawing/S_CheckPoint_IS.cs
@@ -2,21 +2,12 @@ using UnityEngine;
 
 public class S_CheckPoint_IS : MonoBehaviour
 {
-    private S_PlayerMovement_IS _player;
     private bool _isActivated;
-    
-    private void Start()
-    {
-        if (_player == null)
-        {
-            _player = FindFirstObjectByType<S_PlayerMovement_IS>();
-        }
-    }
     
     private void OnTriggerEnter(Collider other)
     {
         if (_isActivated) return;
         _isActivated = true;
-        _player.SetCheckPoint(this);
+        other.GetComponent<S_PlayerMovement_IS>().SetCheckPoint(this);
     }
 }
